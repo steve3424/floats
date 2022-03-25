a6ef2ff518351c2a4c5b39ecfb8897be944d1ab0
Steve F <stevefrazee123@gmail.com>
2022-03-24 22:01:02 -0400

    exponent shift should be done as uint then cast to int

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ void ToggleBit(uint8_t bit_num, int* value) {
 
 void PrintFloatBits(int* value) {
     // Print actual float number
-    printf("\n%0.17f\n", *(float*)value);
+    printf("%0.17f\n", *(float*)value);
 
     uint32_t is_negative = *value & (uint32_t)1 << 31;
     printf(is_negative ? "[1]        " : "[0]        ");
@@ -22,19 +22,17 @@ void PrintFloatBits(int* value) {
     for(int8_t i = 22; i > -1; --i) {
         printf(*value & (uint32_t)1 << i ? "1" : "0");
     }
-    printf("]");
-    printf("\n");
-
+    printf("]\n");
 
     // Print text
     printf(is_negative ? " -  " : " +  ");
 
-    int8_t exponent_value = *value >> 23;
-    printf("   exp: %d - 127 = %d   ", exponent_value, exponent_value - 127);
+    uint8_t exponent_value = *value >> 23;
+    int8_t exponent_value_shifted = exponent_value - 127;
+    printf("   exp: %d - 127 = %d   \n\n", exponent_value, exponent_value_shifted);
 
     // uint32_t significand = 0x7FFFFF & *value;
     // printf("     sig: %u\n", significand);
-
 }
 
 int main() {
@@ -42,14 +40,14 @@ int main() {
     PrintFloatBits((int*)&f);
     ToggleBit(23, (int*)&f);
     ToggleBit(24, (int*)&f);
-    ToggleBit(25, (int*)&f);
-    ToggleBit(26, (int*)&f);
-    ToggleBit(27, (int*)&f);
-    ToggleBit(28, (int*)&f);
-    ToggleBit(29, (int*)&f);
-    PrintFloatBits((int*)&f);
+    // ToggleBit(25, (int*)&f);
+    // ToggleBit(26, (int*)&f);
+    // ToggleBit(27, (int*)&f);
+    // ToggleBit(28, (int*)&f);
+    // ToggleBit(29, (int*)&f);
+    // ToggleBit(30, (int*)&f);
     // ToggleBit(21, (int*)&f);
-    // PrintFloatBits((int*)&f);
+    PrintFloatBits((int*)&f);
 
     return 0;
 }
