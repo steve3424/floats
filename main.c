7cc0823d391ed74c927dc0841c1dc7454c2d2923
Steve F <stevefrazee123@gmail.com>
2022-03-24 21:40:25 -0400

    change print bits, toggle bits

--- main.c
+++ main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdint.h>
 
-// void SetBit(uint8_t on_or_off, uint8_t bit_num, float* value) {
-
-// }
+void ToggleBit(uint8_t bit_num, int* value) {
+    *value = *value ^ ((uint32_t)1 << bit_num);    
+}
 
 void PrintFloatBits(int* value) {
-    printf(*value & (uint32_t)1 << 31 ? "[1]" : "[0]");
+    // Print actual float number
+    printf("\n%0.17f\n", *(float*)value);
 
-    printf(" [");
+    uint32_t is_negative = *value & (uint32_t)1 << 31;
+    printf(is_negative ? "[1]        " : "[0]        ");
+
+    printf("[");
     for(int8_t i = 30; i > 22; --i) {
         printf(*value & (uint32_t)1 << i ? "1" : "0");
     }
-    printf("] ");
+    printf("]        ");
 
     printf("[");
     for(int8_t i = 22; i > -1; --i) {
@@ -21,13 +25,31 @@ void PrintFloatBits(int* value) {
     printf("]");
     printf("\n");
 
-    printf("%0.5f\n", *(float*)value);
+
+    // Print text
+    printf(is_negative ? " -  " : " +  ");
+
+    int8_t exponent_value = *value >> 23;
+    printf("   exp: %d - 127 = %d   ", exponent_value, exponent_value - 127);
+
+    // uint32_t significand = 0x7FFFFF & *value;
+    // printf("     sig: %u\n", significand);
+
 }
 
 int main() {
-
-    float f = 0.15625f;
+    float f = 0.0f;
     PrintFloatBits((int*)&f);
+    ToggleBit(23, (int*)&f);
+    ToggleBit(24, (int*)&f);
+    ToggleBit(25, (int*)&f);
+    ToggleBit(26, (int*)&f);
+    ToggleBit(27, (int*)&f);
+    ToggleBit(28, (int*)&f);
+    ToggleBit(29, (int*)&f);
+    PrintFloatBits((int*)&f);
+    // ToggleBit(21, (int*)&f);
+    // PrintFloatBits((int*)&f);
 
     return 0;
 }
