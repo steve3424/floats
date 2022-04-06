5da6deed02ee9a2b89ced19303b4fd1d953113b9
Steve F <stevefrazee123@gmail.com>
2022-04-06 17:27:26 -0400

    use float struct instead of pointers and casting, change printing

--- main.c
+++ main.c
@@ -1,53 +1,67 @@
 #include <stdio.h>
 #include <stdint.h>
 
-void ToggleBit(uint8_t bit_num, int* value) {
-    *value = *value ^ ((uint32_t)1 << bit_num);    
+typedef struct {
+    union {
+        uint32_t i;
+        float f;
+    };
+} FloatView;
+
+void ToggleBit(uint8_t bit_num, FloatView* f) {
+    f->i = f->i ^ (1 << bit_num);    
 }
 
-void PrintFloatBits(int* value) {
+void PrintFloatBits(FloatView f, uint8_t decimal_places) {
     // Print actual float number
-    printf("%0.17f\n", *(float*)value);
+    printf("%0.*f\n", decimal_places, f.f);
 
-    uint32_t is_negative = *value & (uint32_t)1 << 31;
-    printf(is_negative ? "[1]        " : "[0]        ");
+    uint32_t is_negative = f.i & (1 << 31);
+    printf(is_negative ? "[1] " : "[0] ");
 
     printf("[");
-    for(int8_t i = 30; i > 22; --i) {
-        printf(*value & (uint32_t)1 << i ? "1" : "0");
+    for(int i = 30; i > 22; --i) {
+        printf(f.i & (1 << i) ? "1" : "0");
     }
-    printf("]        ");
+    printf("] ");
 
     printf("[");
-    for(int8_t i = 22; i > -1; --i) {
-        printf(*value & (uint32_t)1 << i ? "1" : "0");
+    for(int i = 22; i > -1; --i) {
+        printf(f.i & (1 << i) ? "1" : "0");
     }
     printf("]\n");
 
-    // Print text
+    // // Print text
     printf(is_negative ? " -  " : " +  ");
 
-    uint8_t exponent_value = *value >> 23;
-    int8_t exponent_value_shifted = exponent_value - 127;
-    printf("   exp: %d - 127 = %d   \n\n", exponent_value, exponent_value_shifted);
+    uint8_t exponent_value = f.i >> 23;
+    int8_t exponent_value_biased = exponent_value == 0 ? -126 : exponent_value - 127;
+    // Print different number of spaces based on number of decimal digits in biased exponent
+    if(exponent_value_biased < 10) {
+        printf("   2^%d     ", exponent_value_biased);
+    }
+    else if(exponent_value_biased < 100) {
+        printf("   2^%d    ", exponent_value_biased);
+    }
+    else {
+        printf("   2^%d   ", exponent_value_biased);
+    }
 
-    // uint32_t significand = 0x7FFFFF & *value;
-    // printf("     sig: %u\n", significand);
+    uint32_t mantissa_as_int = f.i & 8388607;
+    float two_to_the_negative_23 = 0.00000011920928955078125f;
+    float decimal_part = (float)mantissa_as_int * two_to_the_negative_23;
+    if(exponent_value != 0) {
+        decimal_part += 1.0f;
+    }
+    printf("%.23f", decimal_part);
+
+    printf("\n");
 }
 
 int main() {
-    float f = 0.0f;
-    PrintFloatBits((int*)&f);
-    ToggleBit(23, (int*)&f);
-    ToggleBit(24, (int*)&f);
-    // ToggleBit(25, (int*)&f);
-    // ToggleBit(26, (int*)&f);
-    // ToggleBit(27, (int*)&f);
-    // ToggleBit(28, (int*)&f);
-    // ToggleBit(29, (int*)&f);
-    // ToggleBit(30, (int*)&f);
-    // ToggleBit(21, (int*)&f);
-    PrintFloatBits((int*)&f);
+    FloatView f1;
+    f1.f = 0.1f;
+    PrintFloatBits(f1, 100);
 
     return 0;
 }
